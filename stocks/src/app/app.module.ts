4bfb9b73c5a79ca33e76b99ab6eed46f2068fa9f
Johnny B <jrbostonjr@gmail.com>
2018-06-25 05:00:46 -0400

    completed basic stocks display app with dashboard and manage components, as well as routing

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { StocksService } from './services/stocks.service';
@@ -9,6 +10,7 @@ import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ManageComponent } from './components/manage/manage.component';
 
+import { AppRoutes } from './app.routes';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +20,9 @@ import { ManageComponent } from './components/manage/manage.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutes
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
--- stocks/src/app/components/manage/manage.component.ts
+++ stocks/src/app/components/manage/manage.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { StocksService } from '../../services/stocks.service';
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css']
 })
-export class ManageComponent implements OnInit {
+export class ManageComponent {
 
-  constructor() { }
+  symbols: Array<string>;
+  stock: string;
 
-  ngOnInit() {
-  }
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
+   }
+
+   add() {
+     this.symbols = this.service.add(this.stock.toUpperCase());
+     this.stock = '';
+   }
+   remove (symbol) {
+     this.symbols = this.service.remove(symbol);
+   }
+
 
 }
