cf9acdc845566b71b171ed37ecc418120a02af09
Johnny B <jrbostonjr@gmail.com>
2018-06-25 04:10:53 -0400

    updated stocks app with functional dashboard component and added manage component

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { StocksService } from './services/stocks.service';
 
 import { AppComponent } from './app.component';
-
+import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
